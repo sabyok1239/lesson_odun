eae8e50df9c9b18080b299333e9f496cafccd463
sabyok1239 <sashabastan9@gmail.com>
2026-02-01 14:43:40 +0200

    My first class about friends

---
+++ myClass.py
@@ -0,0 +1,22 @@
+class Student:
+    def __init__(self, name, age, height, everageMark):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.everageMark = everageMark
+
+
+mila = Student(name = "Mila", age = 13, height=167, everageMark=12)
+print(mila.name, mila.age, mila.height, mila.everageMark)
+
+Kyrylo = Student(name = "Kyrylo", age= 12, height=152, everageMark= 12)
+print(Kyrylo.name, Kyrylo.age, Kyrylo.height, Kyrylo.everageMark)
+
+Oleksandr_B = Student(name = "Oleksandr_B", age= 14, height=174, everageMark= 12)
+print(Oleksandr_B.name, Oleksandr_B.age, Oleksandr_B.height, Oleksandr_B.everageMark)
+
+Oleksandr_K = Student(name = "Oleksandr_K", age= 14, height=180, everageMark= 12)
+print(Oleksandr_K.name,Oleksandr_K.age, Oleksandr_K.height, Oleksandr_K.everageMark)
+
+
+
